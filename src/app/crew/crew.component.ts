57ea8448fd538a9b0fd127c2d378456ebcdf0989
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2020-12-17 20:26:24 -0600

    finished studio 3 part 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,19 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate){
+    if (this.crew.includes(candidate)){
+      this.inCrew = true;
+    } else {
+      this.inCrew = false;
+    }
+
+    if (this.inCrew){
+      let i = this.crew.indexOf(candidate);
+      this.crew.splice(i,1);
+    } else if (!this.inCrew && this.crew.length < 3){
+      this.crew.push(candidate);
+    }
+  }
 
 }
